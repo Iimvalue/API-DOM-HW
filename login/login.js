2ad2dfbfe35aa7942fb4570d203535f2d0453dc5
Abdulrahman <1xvlux1@gmail.com>
2025-05-12 01:38:32 +0300

    add check logined in or not

--- login/login.js
+++ login/login.js
@@ -1,12 +1,17 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+localStorage.getItem("user-name") != null
+  ? (window.location.href = "../index.html")
+  : "";
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    var userName = document.getElementById('userName').value.trim();
-    var password = document.getElementById('password').value;
+    var userName = document.getElementById("userName").value.trim();
+    var password = document.getElementById("password").value;
     var storedUserName = localStorage.getItem("user-name");
     var storedPassword = localStorage.getItem("password");
     if (userName === storedUserName && password === storedPassword) {
-        window.location.href = '../index.html';
+      window.location.href = "../index.html";
     } else {
-        alert('Invalid full name or password');
+      alert("Invalid full name or password");
     }
-});
+  });
