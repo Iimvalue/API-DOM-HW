6a7630d85fc616d7c6ebd9c488273eff954347ea
Abdulrahman <1xvlux1@gmail.com>
2025-05-12 01:56:28 +0300

    Display none for small screens

--- login/login.js
+++ login/login.js
@@ -1,18 +1,16 @@
-localStorage.getItem("sign-up") == "from-sign-up"
-  ? ""
-  : (window.location.href = "../index.html");
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (e) {
-    e.preventDefault()
-    var userName = document.getElementById("userName").value.trim();
-    var password = document.getElementById("password").value;
-    var storedUserName = localStorage.getItem("user-name");
-    var storedPassword = localStorage.getItem("password");
-    if (userName === storedUserName && password === storedPassword) {
-      window.location.href = "../index.html";
-    } else {
-      alert("Invalid full name or password");
-    }
-    localStorage.setItem("sign-up", "from-sign-in")
-  });
+localStorage.getItem("sign-up") != "from-sign-up"
+  ? (window.location.href = "../index.html")
+  : "";
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  var userName = document.getElementById("userName").value.trim();
+  var password = document.getElementById("password").value;
+  var storedUserName = localStorage.getItem("user-name");
+  var storedPassword = localStorage.getItem("password");
+  if (userName === storedUserName && password === storedPassword) {
+    window.location.href = "../index.html";
+  } else {
+    alert("Invalid full name or password");
+  }
+  localStorage.setItem("sign-up", "from-sign-in");
+});
